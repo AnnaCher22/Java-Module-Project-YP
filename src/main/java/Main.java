d179eca196effe377b60962d8f2ac2873bec862c
AnnaCher <anjuta.cher@inbox.ru>
2024-10-19 17:51:58 +0300

    ver 1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+        for(int i = 1; i < 4; i++) {
+            System.out.println("Введите название машины №" + i);
+            String nameCar = scanner.next();
+            int speedCar;
+            while (true) {
+                System.out.print("Введите скорость машины №" + i);
+                speedCar = scanner.nextInt();
+                if (speedCar > 0 && speedCar <= 250) {
+                    break;
+                } else {
+                    System.out.println("Некорректная скорость. Попробуйте еще раз.");
+                }
+            }
+
+            Car car = new Car(nameCar, speedCar);
+            race.addCar(car);
+        }
+
+        String leader = race.getLeader();
+        System.out.println("Самая быстрая машина: " + race.getLeader());
+
+        scanner.close();
+
     }
 }
